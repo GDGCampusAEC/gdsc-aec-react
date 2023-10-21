3aee09a46f03d4ee567e91f8412c74b1929f1340
shivamshaw07 <shivamshaw9005@gmail.com>
2023-10-21 16:41:31 +0530

    Added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
-    <div className='bg-red-600'>GDSC</div>
+    <div className='h-[100%] w-[100vw] overflow-x-hidden box-border'>
+      <Navbar/>
+    </div>
   )
 }
 
