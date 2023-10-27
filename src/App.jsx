7d64d6982321cd945b54db64c0e2d049134bb73c
Rajdip Sinh <rajdips834@gmail.com>
2023-10-27 15:03:06 +0530

    Card design pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ContactUs from "./components/contact/ContactUs";
 import Footer from "./components/footer/Footer";
+import Team from "./components/team/Team";
 
 function App() {
   return (
     <div>
+      <Team />
       <ContactUs />
       <Footer />
     </div>
